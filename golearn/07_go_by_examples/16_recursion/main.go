e303a542da2d22005cfac0037697a78f547ffeb2
dongjeremy <dongjeremy2020@163.com>
2020-03-24 13:22:10 +0800

    add go by example.

---
+++ golearn/07_go_by_examples/16_recursion/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Go 支持 递归。 这里是一个经典的阶乘示例。
+
+// fact 函数在到达 fact(0) 前一直调用自身。
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(fact(7))
+}
+
+/* $ go run recursion.go
+5040 */
